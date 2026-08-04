cac235a42d814b44ed78786c2f216abee74e41f4
Rahul Agarwal <rahul.agarwal.workspace@gmail.com>
2026-08-04 20:08:35 +0530

    Add AB Balance [A] from Codeforces

---
+++ 1606/A - AB Balance/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        string s;
+        cin >> s;
+ 
+        if (s.front() != s.back()) {
+            s.front() = s.back();
+        }
+ 
+        cout << s << endl;
+    }
+ 
+    return 0;
+}
